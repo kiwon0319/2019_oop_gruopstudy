8eb1f92134613b94b075fc2bec03dce0e753c4fb
kiwon <kiwon0319@gmail.com>
2019-05-04 04:39:26 +0900

    reupload playable.java

--- TRPG_MC/src/Playable.java
+++ TRPG_MC/src/Playable.java
@@ -1,41 +1,44 @@
 public class Playable extends Players{
-
-    public Playable (String _name){
-        this.name = _name;
-    }
-
     @Override
     void makeStat() {
-        int san = pow * 5;
-            if(san >99)
-                san = 99 - cp;
-        int cp = 0; //크툴루 포인트
-        int sanMax = pow * 5;
+        cp = 0;
 
-        int str = Dice.roll(3, 6);
-        int con = Dice.roll(3, 6);
-        int siz = Dice.roll(2,6) + 6;
-        int intel = Dice.roll(2, 6) + 6;
-        int pow = Dice.roll(3, 6);
-        int dex = Dice.roll(3, 6);
-        int app = Dice.roll(3, 6);
-        int edu = Dice.roll(3, 6) + 6;
+        str = Dice.roll(3, 6);
+        con = Dice.roll(3, 6);
+        siz = Dice.roll(2, 6) + 6;
+        intel = Dice.roll(2, 6) + 6;
+        pow = Dice.roll(3, 6);
+        dex = Dice.roll(3, 6);
+        app = Dice.roll(3, 6);
+        edu = Dice.roll(3, 6) + 6;
 
-        int idea = intel * 5;
-            if(idea > 99)
-                idea = 99;
-        int luck = pow * 5;
-            if(luck > 99)
-                luck = 99;
-        int know = edu * 5;
-            if(know > 99)
-                know = 99;
+        idea = intel * 5;
+        luck = pow * 5;
+        know = edu * 5;
 
-        int maxHp = (con + siz) % 2;;
-        int hp = maxHp;
-        int maxMp = pow;
-        int mp = maxMp;
+        if(idea > 99){
+            idea = 99;
+        }
+        if(luck > 99){
+            luck = 99;
+        }
+        if(know > 99){
+            know = 99;
+        }
 
-        int db = siz + str;
+        db = siz + str;
+
+        maxHp = (con + siz) / 2;
+        hp = maxHp;
+        maxMp = pow * 5;
+        mp = maxMp;
+
+        sanMax = pow * 5;
+        if(sanMax > 99 - cp){
+            sanMax = 99 - cp;
+        }
+        san = sanMax;
+
+
     }
 }
