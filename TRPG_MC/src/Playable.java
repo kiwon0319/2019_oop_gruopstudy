70c84fdb46152eb5020cdd088524eb21fac05889
장태영 <jty4342@naver.com>
2019-05-09 16:25:58 +0900

    1

---
+++ TRPG_MC/src/Playable.java
@@ -0,0 +1,34 @@
+public class Playable extends Players {
+    @Override
+    void makeStat(){
+
+        str=Dice.roll(3,6);
+        con=Dice.roll(3,6);
+        siz=Dice.roll(2,6)+6;
+        intel=Dice.roll(2,6)+6;
+        pow=Dice.roll(3,6);
+        dex=Dice.roll(3,6);
+        app=Dice.roll(3,6);
+        edu=Dice.roll(3,6)+6;
+
+
+        idea=intel*5;
+        if(idea>=100){
+            idea=99;
+        }
+        luck=pow*5;
+        if(luck>=100){
+            luck=99;
+        }
+        know=edu*5;
+        if(know>=100){
+            know=99;
+        }
+
+        db=siz+str;
+        hp=(con+siz)/2;
+        mp=maxMp=pow;
+    }
+}
+
+
