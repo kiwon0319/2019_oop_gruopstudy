2b16dbb7c1f3c2e6b53defae39427a9da703b38d
Unknown <jaeyeong221@gmail.com>
2019-05-17 16:21:20 +0900

    Npc, Main

--- TRPG_MC/src/Main.java
+++ TRPG_MC/src/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int players = 5;
-        String playername[] = {"a", "b", "c", "d", "e"};
-        Playable p[] = new Playable[5];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("플레이어 수를 입력하세요");
+        int players = scan.nextInt();
+
+        Playable p[] = new Playable[players];
 
         for (int i = 0; i < players; i++) {
-            p[i] = new Playable(playername[i]);
+            System.out.println("플레이어 이름을 입력하세요");
+            String playername = scan.nextLine();
+            p[i] = new Playable(playername);
             p[i].makeStat();
         }
     }
