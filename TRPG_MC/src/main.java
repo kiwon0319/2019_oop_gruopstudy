8c3c9108b54c704bd79f4627af652012ec2fd553
Elieka <dudwns19557@gmail.com>
2019-05-17 16:38:42 +0900

    안ㄴ어미

---
+++ TRPG_MC/src/main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(string[] args)
+    {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("플레이어 수를 입력하세요");
+        int players = scan.nextInt();
+
+        Playable p[] = new Playable[players];
+        for (int i = 0; i < players; i++) {
+
+            System.out.println("플레이어 이름을 입력하세요");
+            String playername = scan.nextLine();
+            p[i] = new Playable(playername);
+            p[i].makestat();
+        }
+    }
+}
