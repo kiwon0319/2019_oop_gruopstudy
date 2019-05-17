ad282352218f9a7df6d293c0c1e1497ca8b836ff
walt4771 <walt4771@gmail.com>
2019-05-17 16:46:14 +0900

    NULL
    끼에에에엨

--- TRPG_MC/src/Main.java
+++ TRPG_MC/src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        int players;
+        Scanner scan = new Scanner(System.in);
+
+        players = scan.nextInt();
+        // String playername[] = {"a", "b", "c", "d", "e"};
+        Playable p[] = new Playable[5];
+
+        for (int i = 0; i < players; i++) {
+            String playername;
+            playername = scan.nextLine();
+            p[i] = new Playable(playername);
+            p[i].makeStat();
+        }
+    }
 }
